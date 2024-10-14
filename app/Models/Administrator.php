232dc49ae86f7a835b34ee447c9766613b007c4b
dc <damnyan@gmail.com>
2024-10-14 12:28:35 +0800

    Update casting for password

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -2,11 +2,9 @@
 
 namespace Modules\Administrator\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 use Modules\Administrator\Database\Factories\AdministratorFactory;
 use Modules\File\Casts\UploadFile;
@@ -49,6 +47,7 @@ class Administrator extends Authenticatable
     {
         return [
             'face_url' => UploadFile::class,
+            'password' => 'hashed',
         ];
     }
 
@@ -60,17 +59,5 @@ class Administrator extends Authenticatable
     protected static function newFactory(): AdministratorFactory
     {
         return AdministratorFactory::new();
-    }
-
-    /**
-     * Passsword
-     *
-     * @return Attribute
-     */
-    protected function password(): Attribute
-    {
-        return Attribute::make(
-            set: fn ($value) => Hash::make($value),
-        );
     }
 }
